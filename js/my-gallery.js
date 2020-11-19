80763f122d053bf3989903e51abb7ddbc2627abe
Volodymyr Vorona <volodimir.vorona@gmail.com>
2020-11-19 02:22:05 +0200

    Додає реалізацію модалки

--- js/my-gallery.js
+++ js/my-gallery.js
@@ -1,20 +1,18 @@
-import gallerySourseFiles from "./gallery-items.js";
-
-const galleryListRef = document.querySelector(".js-gallery");
-// galleryListRef.addEventListener("click", );
+import gallerySourseFiles from './gallery-items.js';
 
 function createElement(element) {
-  const galleryItemRef = document.createElement("li");
-  galleryItemRef.classList.add("gallery__item");
+  const galleryItemRef = document.createElement('li');
+  galleryItemRef.classList.add('gallery__item');
 
-  const galleryLinkRef = document.createElement("a");
-  galleryLinkRef.classList.add("gallery__link");
+  const galleryLinkRef = document.createElement('a');
+  galleryLinkRef.classList.add('gallery__link');
   galleryLinkRef.href = element.original;
 
-  const galleryImageRef = document.createElement("img");
-  galleryImageRef.classList.add("gallery__image");
+  const galleryImageRef = document.createElement('img');
+  galleryImageRef.classList.add('gallery__image');
   galleryImageRef.src = element.preview;
-  galleryImageRef.setAttribute("data-source", `${element.original}`);
+  // galleryImageRef.setAttribute('data-source', element.original);
+  galleryImageRef.dataset.source = element.original;
   galleryImageRef.alt = element.description;
 
   galleryLinkRef.appendChild(galleryImageRef);
@@ -23,6 +21,48 @@ function createElement(element) {
   return galleryItemRef;
 }
 
-const gallerySourseArr = gallerySourseFiles.map((item) => createElement(item));
-
+const gallerySourseArr = gallerySourseFiles.map(item => createElement(item));
+const galleryListRef = document.querySelector('.js-gallery');
 galleryListRef.append(...gallerySourseArr);
+
+const ref = {
+  backDrop: document.querySelector('.js-lightbox'),
+  modalImage: document.querySelector('.lightbox__image'),
+  closeBtn: document.querySelector('button[data-action="close-lightbox"]'),
+};
+
+galleryListRef.addEventListener('click', onGalleryClick);
+ref.closeBtn.addEventListener('click', hideModal);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  const target = event.target;
+  const currentTarget = event.currentTarget;
+
+  if (target === currentTarget) {
+    return;
+  }
+
+  ref.modalImage.src = target.dataset.source;
+  ref.modalImage.alt = target.getAttribute('alt');
+
+  openModal();
+}
+
+function openModal() {
+  ref.backDrop.classList.add('is-open');
+  window.addEventListener('keydown', onKeyDown);
+}
+
+function hideModal() {
+  ref.backDrop.classList.remove('is-open');
+  ref.modalImage.src = '';
+  window.removeEventListener('keydown', onKeyDown);
+}
+
+function onKeyDown(event) {
+  if (event.code === 'Escape') {
+    hideModal();
+  }
+}
