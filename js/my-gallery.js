4eaa66d47ee14d3f7b246f90cef9135bde590fe2
Volodymyr Vorona <volodimir.vorona@gmail.com>
2020-11-20 19:16:26 +0200

    Додає спробу зробити перелистування галереї

--- js/my-gallery.js
+++ js/my-gallery.js
@@ -1,5 +1,7 @@
 import gallerySourseFiles from './gallery-items.js';
 
+// створення галереї
+
 function createElement(element) {
   const galleryItemRef = document.createElement('li');
   galleryItemRef.classList.add('gallery__item');
@@ -13,6 +15,7 @@ function createElement(element) {
   galleryImageRef.src = element.preview;
   // galleryImageRef.setAttribute('data-source', element.original);
   galleryImageRef.dataset.source = element.original;
+  galleryImageRef.dataset.index = element.index;
   galleryImageRef.alt = element.description;
 
   galleryLinkRef.appendChild(galleryImageRef);
@@ -22,6 +25,7 @@ function createElement(element) {
 }
 
 const gallerySourseArr = gallerySourseFiles.map(item => createElement(item));
+
 const galleryListRef = document.querySelector('.js-gallery');
 galleryListRef.append(...gallerySourseArr);
 
@@ -30,6 +34,8 @@ const ref = {
   modalImage: document.querySelector('.lightbox__image'),
   closeBtn: document.querySelector('button[data-action="close-lightbox"]'),
 };
+
+// підключення великого зображення
 
 galleryListRef.addEventListener('click', onGalleryClick);
 ref.closeBtn.addEventListener('click', hideModal);
@@ -50,6 +56,8 @@ function onGalleryClick(event) {
   openModal();
 }
 
+// відкриття і закриття модалки
+
 function openModal() {
   ref.backDrop.classList.add('is-open');
   window.addEventListener('keydown', onKeyDown);
@@ -61,8 +69,23 @@ function hideModal() {
   window.removeEventListener('keydown', onKeyDown);
 }
 
+// дії, при натисненні кнопок
+
 function onKeyDown(event) {
   if (event.code === 'Escape') {
     hideModal();
+  }
+
+  if (event.code === 'ArrowRight') {
+    let prevIndex = +event.target.firstChild.dataset.index;
+    let nextIndex = +event.target.firstChild.dataset.index + 1;
+    console.log(prevIndex, nextIndex);
+
+    if (nextIndex + 1 === gallerySourseFiles.length) {
+      return;
+    }
+    prevIndex += 1;
+    ref.modalImage.src = gallerySourseFiles[nextIndex].original;
+    console.log(gallerySourseFiles[nextIndex]);
   }
 }
